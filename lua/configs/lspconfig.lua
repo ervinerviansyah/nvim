17caf8737f8fd603b0cd0b612676e573af88cfe4
ervinerviansyah <ervin.erviansyah2003@gmail.com>
2025-10-15 18:33:46 +0700

    Add lsp and formatters

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,34 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "bacon_ls",
+  "bashls",
+  "biome",
+  "clangd",
+  "csharp_lsp",
+  "css_variables",
+  "cssmodules_ls",
+  "docker_language_server",
+  "eslint",
+  "fish_lsp",
+  "hyprls",
+  "intelephense",
+  "jsonls",
+  "laravel_ls",
+  "lua_ls",
+  "luau_lsp",
+  "postgres_lsp",
+  "pyright",
+  "pylsp",
+  "ruff",
+  "rust_analyzer",
+  "stylua",
+  "tailwindcss",
+  "ts_ls",
+  "vimls",
+}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
